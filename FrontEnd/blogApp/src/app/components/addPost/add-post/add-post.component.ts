559106ee2bba6d04b8f8b6e532350c99a7c9b20f
Azeem Yoonus <azeemyoonus4@gmail.com>
2021-08-17 19:25:57 +0530

    list posted post

--- FrontEnd/blogApp/src/app/components/addPost/add-post/add-post.component.ts
+++ FrontEnd/blogApp/src/app/components/addPost/add-post/add-post.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 import { PostsService } from 'src/app/services/posts/posts.service';
-PostsService
+
 @Component({
   selector: 'app-add-post',
   templateUrl: './add-post.component.html',
@@ -9,7 +10,8 @@ PostsService
 })
 export class AddPostComponent implements OnInit {
 
-  constructor(private postService: PostsService) { }
+  constructor(private postService: PostsService,
+    private route:Router) { }
 
   ngOnInit(): void {
   }
@@ -18,7 +20,7 @@ export class AddPostComponent implements OnInit {
     console.log(addpostForm.value);
     this.postService.addPost(addpostForm.value).subscribe((data)=>{
       console.log(data);
-      
+      this.route.navigate(['home']);
     })
   }
 }
--- FrontEnd/blogApp/src/app/components/home/home/home.component.ts
+++ FrontEnd/blogApp/src/app/components/home/home/home.component.ts
@@ -7,15 +7,17 @@ import { PostsService } from 'src/app/services/posts/posts.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  
+  posts:any;
+
   constructor(private postService :PostsService) { }
-posts:any;
   ngOnInit(): void {
     this.getAllPosts()
+    console.log("this",this.posts)
   }
   getAllPosts(){
-   this.postService.getAllPosts().subscribe((data)=>{
-     console.log(data)
+   this.postService.getAllPosts().subscribe((data: any)=>{
+     this.posts=data.response;
+     console.log(this.posts)
    })
     // console.log(this.posts);
   }
