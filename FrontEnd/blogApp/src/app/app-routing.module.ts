bd418cc07c70aa006cb9fa5c9af9bad53dcf7e51
Azeem Yoonus <azeemyoonus4@gmail.com>
2021-08-18 01:04:03 +0530

    routing temporary changed

--- FrontEnd/blogApp/src/app/app-routing.module.ts
+++ FrontEnd/blogApp/src/app/app-routing.module.ts
@@ -6,9 +6,9 @@ import { LoginComponent } from './components/login/login.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' }, 
-  { path: 'login', component: LoginComponent },
-  { path: 'signUp', component: SignUpComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }, 
+  // { path: 'login', component: LoginComponent },
+  // { path: 'signUp', component: SignUpComponent },
   { path: 'home', component: HomeComponent },
   { path: 'addPost', component: AddPostComponent },
   { path: 'updatePost/:id', component: AddPostComponent },
